d60b7f909ce15619548bda32db9f374dae62562f
Daniel Maděra <madera.dan@gmail.com>
2018-01-25 09:05:59 +0100

    Add user dirs

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cp .bashrc ~/ -fv
 cp .bash_aliases ~/ -fv
 cp .bash_logout ~/ -fv
 cp .Xdefaults ~/ -fv
+cp .config/user-dirs.* ~/.config/ -fv
 
 echo "Installing desktop files, requires sudoer"
 for f in .local/share/applications/*.desktop
