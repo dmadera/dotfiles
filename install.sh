d4bf64550903156dde3e2b0307682e6bc9d56c25
Daniel Maděra <madera.dan@gmail.com>
2018-01-26 14:45:06 +0100

    Rename homedirs to lowercase

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+echo "Renaming and removing home dirs"
+if [ -d ~/downloads  ] ; then
+  rm ~/Documents ~/Pictures ~/Videos -r
+  mv ~/Desktop ~/desktop
+  mv ~/Downloads ~/downloads
+  mv ~/Music ~/media
+  mv ~/Public ~/public
+  mv ~/Templates ~/templates
+fi
+
 echo "Installing dotfiles to home dir"
 
 cp .bashrc ~/ -fv
