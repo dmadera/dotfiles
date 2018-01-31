e44866aa41ef54a81ce04a69982e1b649eba1ed8
Daniel Maděra <madera.dan@gmail.com>
2018-01-31 14:48:39 +0100

    Extend init install

--- install.sh
+++ install.sh
@@ -1,30 +1,29 @@
-echo "Renaming and removing home dirs"
-if [ ! -d ~/downloads  ] ; then
-  rm ~/Documents ~/Pictures ~/Videos -r
-  mv ~/Desktop ~/desktop
-  mv ~/Downloads ~/downloads
-  mv ~/Music ~/media
-  mv ~/Public ~/public
-  mv ~/Templates ~/templates
+#!/bin/bash
 
-  # git already must be installed
-  git config --global user.name "Daniel Maděra"
-  git config --global user.email "madera.dan@gmail.com"
-  git config --global core.editor vim
+if [ $EUID -ne 0  ]; then
+  echo "Not running as root"
+  exit 2
 fi
 
-echo "Installing dotfiles to home dir"
+if [ ! -f /home/daniel/.ssh/id_rsa.pub ]; then
+  echo "Generate ssh private/public key and add it to github.com profile"
+  exit 2
+fi
 
-cp .bashrc ~/ -fv
-cp .bash_aliases ~/ -fv
-cp .bash_logout ~/ -fv
-cp .Xdefaults ~/ -fv
-cp .config/user-dirs.* ~/.config/ -fv
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | tee /etc/apt/sources.list.d/google-chrome.list
 
-echo "Installing desktop files, requires sudoer"
+apt update
+apt upgrade
+apt install build-essential cmake python-dev python3-dev python3-pip
+apt install ruby ruby-dev git
+apt install vim vim-gtk vim-gnome google-chrome-stable git
+
+su daniel ./install_user.sh
+
 for f in .local/share/applications/*.desktop
 do
-  sudo desktop-file-install $f
+  desktop-file-install $f
 done
 
 echo "Done."
---
+++ install_user.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+tmp_dir=$PWD
+
+git config --global user.name "Daniel Maděra"
+git config --global user.email "madera.dan@gmail.com"
+git config --global core.editor vim
+
+echo "Installing VIM config"
+cd ~/
+git clone --recursive git@github.com:daniel-madera/.vim.git
+echo ":source ~/.vim/startup.vim" > ~/.vimrc
+
+# set up VIM plugins
+~/.vim/bundle/YouCompleteMe/install.py
+pip3 install --user powerline-status
+cd  ~/.vim/bundle/command-t
+rake make
+
+cd $tmp_dir
+
+echo "Renaming and removing home dirs"
+if [ ! -d ~/downloads  ] ; then
+  rm ~/Documents ~/Pictures ~/Videos -r
+  mv ~/Desktop ~/desktop
+  mv ~/Downloads ~/downloads
+  mv ~/Music ~/media
+  mv ~/Public ~/public
+  mv ~/Templates ~/templates
+fi
+
+echo "Installing dotfiles to home dir"
+
+cp .bashrc ~/ -fv
+cp .bash_aliases ~/ -fv
+cp .bash_logout ~/ -fv
+cp .Xdefaults ~/ -fv
+cp .config/user-dirs.* ~/.config/ -fv
+
