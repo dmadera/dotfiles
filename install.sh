990790e837ac4359b7ad864fd948c872712b6743
Daniel Maděra <madera.dan@gmail.com>
2018-03-15 11:21:26 +0100

    Install nodejs and curl

--- install.sh
+++ install.sh
@@ -27,7 +27,9 @@ apt install build-essential cmake python-dev python3-dev python3-pip -y
 apt install libc6:i386 -y
 apt install ruby ruby-dev git -y
 apt install google-chrome-stable task-print-server-y
-apt install vlc gimp inkscape -y
+apt install vlc gimp inkscape curl -y
+curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+apt-get install -y nodejs
 update-alternatives --set x-terminal-emulator /usr/bin/urxvt
 
 usermod -a -G sudo daniel
