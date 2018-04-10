3ab1b62e54aadd34809f651a86fef3baa365c7f4
Daniel Maděra <madera.dan@gmail.com>
2018-04-10 09:16:55 +0200

    Add xclip package

--- install.sh
+++ install.sh
@@ -27,7 +27,7 @@ dnf install i3 i3status dmenu i3lock xbacklight feh conky -y
 dnf install pavucontrol xrandr py3status -y
 dnf install dnf-plugin-system-upgrade -y
 dnf install vim-enhanced -y
-dnf install google-chrome-stable gimp inkscape curl -y
+dnf install google-chrome-stable gimp inkscape curl xclip -y
 dnf install youtube-dl simple-scan -y
 dnf install cmake make automake gcc gcc-c++ kernel-devel -y
 dnf install ruby ruby-devel rubygem-rake python-devel python3-devel -y
