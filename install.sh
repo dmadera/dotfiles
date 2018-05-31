5af888742f591940002ef6aaf3ba4afdea7cd0f0
Daniel Maděra <madera.dan@gmail.com>
2018-05-31 09:52:07 +0200

    Add package wireshark

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ dnf install google-chrome-stable gimp inkscape curl xclip okular -y
 dnf install youtube-dl simple-scan -y
 dnf install cmake make automake gcc gcc-c++ kernel-devel -y
 dnf install ruby ruby-devel rubygem-rake python-devel python3-devel -y
-dnf install ftp pv p7zip unar unzip nodejs -y
+dnf install ftp pv p7zip unar unzip nodejs wireshark -y
 
 if [ ! -d "/mnt/disk" ]; then
   mkdir /mnt/disk
